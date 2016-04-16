7b4a4cd80ff8b8194ee9276ee7ed5b513b391ad5
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-16 19:35:38 +0200

    Add modular arithmetic

---
+++ ch1/modular.rkt
@@ -0,0 +1,25 @@
+#lang planet neil/sicp
+
+(define (sqr x)
+  (* x x))
+
+
+(define (expmod base exp n)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (sqr (expmod base (/ exp 2) n))
+                    n))
+        (else
+         (remainder (* base (expmod base (- exp 1) n))
+                    n))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (random (- n 1))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
