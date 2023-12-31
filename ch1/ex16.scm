539ab139f576ea29f551485cdae1a53ee146a96f
Marcin Łoś <marcin.los.91@gmail.com>
2023-12-31 19:29:43 +0100

    Fix exercise 16

--- ch1/ex16.scm
+++ ch1/ex16.scm
@@ -1,20 +1,10 @@
 
-
-(define (sqr x)
+(define (square x)
   (* x x))
 
-(define (even? n)
-  (= (remainder n 2) 0))
-
-(define (power b n)
-  (cond ((= n 0) 1)
-        ((even? n) (sqr (power b (/ n 2))))
-        (else (* b (power b (- n 1))))))
-
-(define (power* b n)
-  (define (power-iter n accum)
-    (cond ((= n 0) accum)
-          ((even? n) (power-iter (/ n 2) (sqr accum)))
-          (else (power-iter (- n 1) (* b accum)))))
-  (power-iter n 1))
-
+(define (fast-expt-iter b n)
+  (define (iter k acc a)
+    (cond ((= k 0) acc)
+          ((even? k) (iter (/ k 2) acc (square a)))
+          (else (iter (- k 1) (* a acc) a))))
+  (iter n 1 b))
